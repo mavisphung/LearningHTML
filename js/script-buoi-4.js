6649d200367f43436a21768840e124535f905027
mavisphung <huypc2410@gmail.com>
2020-12-27 13:41:34 +0700

    Added SignUp function

--- js/script-buoi-4.js
+++ js/script-buoi-4.js
@@ -16,9 +16,58 @@ function Cong() {
 		alert("Vui lòng nhập đầy đủ");
 		return;
 	}
+	
 //	hệ cơ số 10
 	a = parseInt(a, 10);
 	b = parseInt(b, 10);
 	alert(a + " + " + b + " = " + (a+b));
 	
+}
+
+function SignUp() {
+	var username = document.getElementById('username').value;
+	var radioNam = document.getElementById('nam');
+	
+	var password = document.getElementById('password').value;
+	var cpassword = document.getElementById('cpassword').value;
+	var email = document.getElementById('email').value;
+	var dob = document.getElementById('dob').value;
+	var phone = document.getElementById('phone').value;
+	var gioitinh;
+	
+	//check empty
+	if (username == "" || password == "" || cpassword == "" || email == "" || dob == "" || phone == "") {
+		alert("Vui lòng nhập đủ");
+		return;
+	}
+	
+	//check ngày
+	var today = new Date;
+	today = Date.parse(today);
+	dob = Date.parse(dob);
+	if (dob > today) {
+		alert("Ngày sinh phải nhỏ hơn ngày hiện hành");
+		return;
+	}
+	
+	//check password
+	if (password.localeCompare(cpassword) != 0) {
+		alert("Password không trùng khớp");
+		return;
+	}
+	
+	//giới tính
+	if (radioNam.checked)
+		gioitinh = "nam";
+	else gioitinh = "nữ";
+	
+	var result = 
+		"Username: " + username + "\n" +
+		"Password: " + password + "\n" +
+		"Confirm password: " + cpassword + "\n" +
+		"Email: " + email + "\n" +
+		"DoB: " + dob + "\n" +
+		"Phone: " + phone + "\n" +
+		"Gender: " + gioitinh + "\n";
+	alert(result);
 }
